38c8924649ec7810099fc00104dff78fa75b83b3
Piyush Jain <piyushjain100201@gmail.com>
2024-12-12 12:07:10 +0530

    Send email

--- pages/Contact Us.py
+++ pages/Contact Us.py
@@ -1,4 +1,20 @@
 import streamlit as st
+import send_email
 
-st.title("Contact Us")
+st.title("Contact Me")
+
+with st.form(key = "email_form"):
+    user_email = st.text_input(label = "Your email address")
+    user_message = st.text_area(label = "Your message")
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+{user_message}
+"""
+    button = st.form_submit_button()
+    if button:
+        send_email.send_email(message)
+        st.info("Your email was sent successfully")
+
 
